72818a6354f452cdc353c886876bb3c982883db3
mgleason <michael.gleason@digitalglobe.com>
2018-06-20 16:33:01 -0600

    trying to fix import issue with py3

--- nbfootprints/__init__.py
+++ nbfootprints/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
-
-from nbfootprints.ops import *
-from nbfootprints.plots import *
+from .ops import *
+from .plots import *
 
 __all__ = ['ops',
            'plots']
